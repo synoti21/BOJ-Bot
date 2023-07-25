807c4f77d1ceeb24efbb52182fd35c9e280e7663
Ahn Jiwan <synoti21@naver.com>
2023-07-25 22:36:53 +0900

    [feat] 우리 아이가 드디어 말을 해요...
    Signed-off-by: Ahn Jiwan <synoti21@naver.com>

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+const dotenv = require("dotenv");
+
 const {Client, GatewayIntentBits, EmbedBuilder} = require('discord.js')
-const client = new Client({ rest, gateway });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
+});
+
+dotenv.config();
 
 client.login(process.env.DISCORD_TOKEN);
 //Discord 모듈 import
@@ -8,10 +19,10 @@ client.once('ready', async () => {
     console.log("BOJ Bot is ready")
 })
 
-client.on('message', async message => {
+client.on('messageCreate', async (message) => {
     console.log("Ping");
     console.log(message.content);
-    if (message.component === '1'){
-        message.channel.send('퐁')
+    if (message.content === "안녕"){
+        message.reply({embeds:[new EmbedBuilder().setTitle("하이").setDescription("안녕").addFields([{name:"나는", value:"봇"}])]})
     }
 })
