9c5e959e41acfefee18e9ad4128b4e70aa0cba64
Ahn Jiwan <synoti21@naver.com>
2023-07-25 22:48:13 +0900

    [chore] Upload package.json & package-lock.json
    Signed-off-by: Ahn Jiwan <synoti21@naver.com>

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ client.on('messageCreate', async (message) => {
     console.log("Ping");
     console.log(message.content);
     if (message.content === "안녕"){
-        message.reply({embeds:[new EmbedBuilder().setTitle("하이").setDescription("안녕").addFields([{name:"나는", value:"봇"}])]})
+        message.reply({embeds:[new EmbedBuilder().setTitle("안녕").setDescription("나를 소개하지").addFields([{name:"이름은 디코봇", value:"직업은 Traveler"}])]})
+    }else if (message.content == "뭐해"){
+        message.reply("너에게 닿는 중")
+    }else if (message.content == "잘가"){
+        message.reply("다신 보지 말자")
     }
 })
