d353beb495f5139256465f49ec3767ce97a84caf
Ahn Jiwan <synoti21@naver.com>
2023-07-29 16:01:27 +0900

    [chore] 'not implemented yet' messages for those who want to use this bot early
    Signed-off-by: Ahn Jiwan <synoti21@naver.com>

--- commands/daily.js
+++ commands/daily.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'Daily Notification',
     execute(message, args) {
-        message.channel.send("일일 문제 알림")
+        message.channel.send("일일 문제 알림 - 구현 중입니다. 조금만 기다려주세요!")
     },
 };
--- commands/register.js
+++ commands/register.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: '아이디 등록',
     execute(message, args) {
-        message.channel.send("아이디 등록")
+        message.channel.send("아이디 등록 - 구현 중입니다. 조금만 기다려주세요!")
     },
 };
