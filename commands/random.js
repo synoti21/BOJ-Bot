8bfae51206cd57b278983d43f4b1d0d051fa596f
Ahn Jiwan <synoti21@naver.com>
2023-07-29 14:24:42 +0900

    [feat] Random problem with solved.ac API
    Signed-off-by: Ahn Jiwan <synoti21@naver.com>

--- commands/random.js
+++ commands/random.js
@@ -1,22 +1,66 @@
-const axios = require('axios');
-async function getRandomProblemURL() {
-    const response = await axios.get('https://www.acmicpc.net/problem/random', {
-        maxRedirects: 0,  // Redirects를 따르지 않도록 설정
-        validateStatus: function (status) {
-            return status >= 200 && status < 303;  // Redirects를 위한 상태 코드를 유효하게 처리
-        },
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'
+const {EmbedBuilder} = require('discord.js')
+const axios = require('axios')
+class randomProb{
+    constructor(problemId, title, level, tags) {
+        this.problemId = problemId
+        this.title = title
+        this.level = level
+        this.tags = tags.map(tag => tag.key)
+    }
+    getLevel(){
+        let probLevel = this.level
+        console.log(`난이도 : ${probLevel}`)
+        if (1 <= probLevel && probLevel <= 5){
+            return `브론즈 ${6-probLevel}`
+        }else if (6 <= probLevel && probLevel <= 10){
+            return `실버 ${11-probLevel}`
+        }else if (11 <= probLevel && probLevel <= 15){
+            return `골드 ${16-probLevel}`
+        }else if (16 <= probLevel && probLevel <= 20){
+            return `플레티넘 ${21-probLevel}`
+        }else if (21 <= probLevel && probLevel<= 25){
+            return `다이아 ${26-probLevel}`
+        }else if (26 <= probLevel && probLevel <= 30){
+            return `루비 ${31-probLevel}`
+        }else{
+            return `레벨 책정 안됨`
         }
+    }
+}
+async function getRandomProblem() {
+    const randomId = Math.floor(Math.random() * (28400 - 1000)) + 1000;
+    const response = await axios.get('https://solved.ac/api/v3/problem/show', {
+        params: {
+            problemId: randomId,
+        },
     });
+    if (response.data.problemId && response.data.titleKo) {
+        return new randomProb(response.data.problemId, response.data.titleKo, response.data.level, response.data.tags);
+    }
 
-    return response.headers.location;  // 리다이렉트된 URL을 반환
+    return getRandomProblem();
 }
 
 module.exports = {
-    name: 'Random Recommandation',
+    name: '문제 랜덤 추천',
     async execute(message, args) {
-        const problemURL = await getRandomProblemURL();
-        message.channel.send(`Here is a random problem: ${problemURL}`);
-    },
+        const randProblem = await getRandomProblem();
+        console.log(randProblem.problemId)
+        console.log(randProblem.title)
+        console.log(randProblem.getLevel())
+        console.log(randProblem.tags)
+        const randProblemMsg = new EmbedBuilder()
+            .setColor(0x3498DB)
+            .setAuthor({name: 'BOJ Bot'})
+            .setTitle("랜덤 문제 입니다.")
+            .addFields(
+                { name: '문제 번호:', value: `${randProblem.problemId}`, inline: false },
+                { name: '문제:', value: `${randProblem.title}`, inline: false },
+                { name: '난이도:', value: `${randProblem.getLevel()}`, inline: false },
+                { name: '알고리즘 분류:', value: randProblem.tags.length > 0 ? `${randProblem.tags.join(', ')}` : '알고리즘 분류가 되어있지 않습니다.', inline: false },
+                { name: '링크', value: `https://www.acmicpc.net/problem/${randProblem.problemId}`,inline: false}
+            )
+            .setTimestamp()
+        message.channel.send({embeds: [randProblemMsg]})
+    }
 };
