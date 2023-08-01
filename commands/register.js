2aeea631f671607e7db162fd2c3ef25c8c4e1c26
Ahn Jiwan <synoti21@naver.com>
2023-08-01 22:42:37 +0900

    [feat] Implemented registration using AWS RDS
    Signed-off-by: Ahn Jiwan <synoti21@naver.com>

--- commands/register.js
+++ commands/register.js
@@ -1,5 +1,40 @@
-function registerId(discordId, bojId){
-    console.log(`백준 아이디 = ${discordId} : ${bojId}`);
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    host: process.env.RDS_ENDPOINT,
+    user: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB,
+    port: 5432
+});
+
+pool.on('error', (err, client) => {
+    console.error('예기치 못한 에러가 발생했습니다.', err);
+});
+
+async function registerId(discordId, bojId){
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+        const { rows } = await client.query('SELECT boj_id FROM discord.registereduser WHERE discord_id = $1', [discordId]);
+
+        if (rows.length > 0) {
+            return "이미 백준 아이디를 등록하셨습니다."
+        }
+
+        await client.query('INSERT INTO discord.registereduser(discord_id, boj_id) VALUES($1, $2)', [discordId, bojId]);
+        await client.query('COMMIT');
+
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.log(error);
+        return "오류가 발생했습니다."
+    } finally {
+        client.release();
+    }
+    return "백준 아이디가 등록되었습니다.";
+
 }
 
 
@@ -18,10 +53,10 @@ module.exports = {
 
         idCollector.on('collect', async msg => {
             const bojId = msg.content;
-            registerId(msg.author, bojId)
+            const queryMessage = await registerId(msg.author.id, bojId)
 
             console.log(msg.content)
-            message.reply("백준 아이디가 등록되었어요!")
+            message.reply(queryMessage)
             idCollector.stop();
         });
 
