3eddf81d8eb1a153c39036617452df17eccd0b3c
Ahn Jiwan <synoti21@naver.com>
2023-08-11 20:08:52 +0900

    [feat] Changed postgreSQL -> mySQL && added comments

--- commands/register.js
+++ commands/register.js
@@ -1,67 +1,79 @@
-const { Pool } = require('pg')
+const mysql = require('mysql2/promise')
 
-const pool = new Pool({
+const pool = mysql.createPool({
     host: process.env.RDS_ENDPOINT,
     user: process.env.RDS_USER,
     password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB,
-    port: 5432
+    database: 'boj_bot_db',
+    port: 3306
 });
 
-pool.on('error', (err, client) => {
-    console.error('예기치 못한 에러가 발생했습니다.', err);
-});
-
-async function registerId(discordId, bojId){
-    const client = await pool.connect();
+const getConnection = async () => { //Pool 커넥션 불러오기
+    try {
+        return await pool.getConnection();
+    } catch (error) {
+        console.error(`connection error : ${error.message}`);
+        return null;
+    }
+}
+async function registerId(discordId, bojId) {
+    const conn = await getConnection(); //DB 연결 구축
 
     try {
-        await client.query('BEGIN');
-        const { rows } = await client.query('SELECT boj_id FROM discord.registereduser WHERE discord_id = $1', [discordId]);
+        await conn.beginTransaction(); //트랜잭션 시작
 
-        if (rows.length > 0) {
-            return "이미 백준 아이디를 등록하셨습니다."
+        console.log("Searching existing ID...")
+        const [rows] = await conn.execute('SELECT boj_id FROM registereduser WHERE discord_id = ?', [discordId]);
+        //현재 등록하고자 하는 백준 ID가 이미 있는지 확인
+        if (rows.length > 0) { //이미 있다면
+            return "이미 백준 아이디를 등록하셨습니다."; //TODO 이 부분은 나중에 백준 ID를 수정 가능하도록 바꿔야 함.
         }
 
-        await client.query('INSERT INTO discord.registereduser(discord_id, boj_id) VALUES($1, $2)', [discordId, bojId]);
-        await client.query('COMMIT');
+        console.log("Adding BOJ ID...")
+        await conn.execute('INSERT INTO registereduser(discord_id, boj_id) VALUES(?, ?)', [discordId, bojId]);
+
+        await conn.commit();
+        return "백준 아이디가 등록되었습니다.";
 
     } catch (error) {
-        await client.query('ROLLBACK');
-        console.log(error);
-        return "오류가 발생했습니다."
+        console.error('Error executing query: ' + error.message);
+        await conn.rollback();
+        return "오류가 발생했습니다.";
+
     } finally {
-        client.release();
+        conn.release();
     }
-    return "백준 아이디가 등록되었습니다.";
-
 }
-
 
 module.exports = {
     name: '아이디 등록',
-    execute(message, userCommandStatus, args) {
-        if (userCommandStatus[message.author.id]) {
+    async execute (message, userCommandStatus, args) {
+        if (userCommandStatus[message.author.id]) { //해당 사용자가 이미 다른 명령어를 실행하고 있는 경우
             return;
         }
 
-        userCommandStatus[message.author.id] = true;
+        userCommandStatus[message.author.id] = true; //명령어를 실행하는 상태로 전환
 
         message.reply(`${message.author.toString()}님. 백준 ID를 입력해주세요!`)
         const botFilter = m => !m.author.bot && m.author.id === message.author.id && !m.content.startsWith('!');
+        //Bot이 자체적으로 전송하는 메시지를 거르고, '!'로 시작하며 해당 명령어를 친 사용자 명령어만 받아야함.
         const idCollector = message.channel.createMessageCollector({filter: botFilter,max:1, time: 20000});
+        //메시지 콜렉터 생성. 최대 한 개의 백준 ID만 받으며 제한시간은 20초
 
         idCollector.on('collect', async msg => {
             const bojId = msg.content;
             const queryMessage = await registerId(msg.author.id, bojId)
 
-            console.log(msg.content)
+            console.log(`Collected Message by ${msg.author.username}: ${msg.content}`)
             message.reply(queryMessage)
+            //백준 ID를 입력했으면 아이디 콜렉터 종료
             idCollector.stop();
         });
 
         idCollector.on('end', collected => {
             userCommandStatus[message.author.id] = false;
+
+            //시간 초과되면 종료
             if (collected.size === 0) {
                 message.reply("아직 입력해주시지 않아 시간이 만료되었어요.");
             }
