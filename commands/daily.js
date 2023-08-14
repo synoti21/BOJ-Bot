201a866149967d36eab5edb38380635eebd60b80
Ahn Jiwan <synoti21@naver.com>
2023-08-14 23:49:32 +0900

    [feat] Implemented scheduled time modification

--- commands/daily.js
+++ commands/daily.js
@@ -6,7 +6,6 @@
 // 4. 형식이 잘못되면 오류 메시지 및 "다시 입력해주세요".
 // 5. 쿼리 실행 후 등록
 
-const { ActionRowBuilder, ButtonBuilder } = require('discord.js');
 const { getConnection } = require('../database/connect');
 
 async function getUserCron(author, message, userCommandStatus){
@@ -19,26 +18,34 @@ async function getUserCron(author, message, userCommandStatus){
         console.log(`returned rows: ${JSON.stringify(rows, null, 2)}`);
 
         if (rows.length > 0) {
-            const buttonRow = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('yes')
-                        .setLabel('예')
-                        .setStyle('Success'),
-                    new ButtonBuilder()
-                        .setCustomId('no')
-                        .setLabel('아니오')
-                        .setStyle('Danger')
-                );
+            const [hour, min] = rows[0].cron.split(' ');
+            message.reply(`${hour}시 ${min}분으로 알림이 설정된 상태입니다. 변경하시겠습니까? (y/n)`);
 
-            message.reply({
-                content: `${rows[0].cron}로 알림이 설정된 상태입니다. 변경하시겠습니까?`,
-                components: [buttonRow]
+            const botFilter = m => !m.author.bot && m.author.id === message.author.id && !m.content.startsWith('!');
+            const responseCollector = message.channel.createMessageCollector({filter: botFilter,max:1, time: 20000});
+
+            responseCollector.on('collect', async msg => {
+                if (msg.content === 'y'){
+                    askForTime(message, userCommandStatus, conn, 1);
+                }else if (msg.content === 'n'){
+                    message.reply("변경을 취소하셨습니다.")
+                }
+                responseCollector.stop();
             });
+
+            responseCollector.on('end', collected => {
+                userCommandStatus[message.author.id] = false;
+
+                //시간 초과되면 종료
+                if (collected.size === 0) {
+                    message.channel.send("아직 입력해주시지 않아 시간이 만료되었어요.");
+                }
+            });
+
             conn.release();
         } else {
             conn.release();
-            askForTime(message, userCommandStatus, conn);
+            askForTime(message, userCommandStatus, conn, 0);
         }
     }catch (error){
         await conn.rollback();
@@ -48,24 +55,28 @@ async function getUserCron(author, message, userCommandStatus){
 }
 
 
-function askForTime(message, userCommandStatus, conn) {
-    message.reply("원하시는 시간을 24시간제로 다음과 같이 입력해주세요. (HH MM), 취소를 원하실 경우 X를 입력해주세요.");
+function askForTime(message, userCommandStatus, conn, isAltering) {
+    message.channel.send("원하시는 시간을 24시간제로 다음과 같이 입력해주세요. (HH MM), 취소를 원하실 경우 '취소'를 입력해주세요.");
     console.log("Asking For time")
 
     const botFilter = m => !m.author.bot && m.author.id === message.author.id && !m.content.startsWith('!');
-
     const idCollector = message.channel.createMessageCollector({filter: botFilter,max:1, time: 20000});
 
     idCollector.on('collect', async msg => {
         const cronMsg = msg.content;
-        const isCronInserted = await insertUserCron(message.author.id, cronMsg, conn)
+        if (cronMsg === '취소'){
+            console.log("Canceled Daily command")
+            message.reply("명령을 취소하셨습니다.")
+            return;
+        }
+        const isCronInserted = await insertUserCron(message.author.id, cronMsg, conn, isAltering)
 
         console.log(`Collected Message by ${msg.author.username}: ${msg.content}, ${isCronInserted}`)
         if (isCronInserted){
             const [hour, min] = cronMsg.split(' ')
-            message.reply(`성공적으로 등록되었습니다. 설정한 시간: ${hour}시 ${min}분`)
+            message.channel.send(`성공적으로 등록되었습니다. 설정한 시간: ${hour}시 ${min}분`)
         }else{
-            message.reply("오류가 발생했습니다.")
+            message.channel.send("오류가 발생했습니다.")
         }
         //백준 ID를 입력했으면 아이디 콜렉터 종료
         idCollector.stop();
@@ -76,23 +87,28 @@ function askForTime(message, userCommandStatus, conn) {
 
         //시간 초과되면 종료
         if (collected.size === 0) {
-            message.reply("아직 입력해주시지 않아 시간이 만료되었어요.");
+            message.channel.send("아직 입력해주시지 않아 시간이 만료되었어요.");
         }
     });
 
 }
 
-async function insertUserCron(discordId, userInput, conn) {
+async function insertUserCron(discordId, userInput, conn, isAltering) {
     const [hour, minute] = userInput.split(' ');
 
     if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
         console.log("Invalid User Format")
         return false;
     }
-    const userCron = `${minute} ${hour}`
+    const userCron = `${hour} ${minute}`
     console.log(userCron)
     try{
-        await conn.execute('INSERT INTO user_cron(discord_id, cron) VALUES(?, ?)', [discordId, userCron]);
+        if (!isAltering){
+            await conn.execute('INSERT INTO user_cron(discord_id, cron) VALUES(?, ?)', [discordId, userCron]);
+        }else{
+            await conn.execute('UPDATE user_cron SET cron = ? where discord_id = ?', [userCron, discordId]);
+        }
+
         const [rows] = await conn.execute('SELECT cron FROM user_cron WHERE discord_id = ?', [discordId])
         console.log(`under returned rows: ${JSON.stringify(rows, null, 2)}`);
         return true;
@@ -101,7 +117,6 @@ async function insertUserCron(discordId, userInput, conn) {
         await conn.rollback();
         return false;
     }
-
 }
 
 module.exports = {
