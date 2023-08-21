9d0048b8f61559417e3e2a74ccdbeee8ed560e28
Ahn Jiwan <synoti21@naver.com>
2023-08-21 21:13:52 +0900

    [feat] Logger module (winston)

---
+++ logger.js
@@ -0,0 +1,61 @@
+const winston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+const process = require('process');
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const logDir = `${process.cwd()}/logs`;
+
+const logFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+    format: combine(
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        label({ label: 'BOJ Bot Log' }),
+        logFormat,
+    ),
+    transports: [
+        new winstonDaily({
+            level: 'info',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir,
+            filename: `%DATE%.log`,
+            maxFiles: 30,
+            zippedArchive: true,
+        }),
+
+        new winstonDaily({
+            level: 'error',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir + '/error',
+            filename: `%DATE%.error.log`,
+            maxFiles: 30,
+            zippedArchive: true,
+        }),
+    ],
+    exceptionHandlers: [
+        new winstonDaily({
+            level: 'error',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir,
+            filename: `%DATE%.exception.log`,
+            maxFiles: 30,
+            zippedArchive: true,
+        }),
+    ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple(),
+            ),
+        }),
+    );
+}
+
+module.exports = logger;
