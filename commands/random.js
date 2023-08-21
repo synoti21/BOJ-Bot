6fc634b2255069ce1be3dbcbca3ebc22b6468546
Ahn Jiwan <synoti21@naver.com>
2023-08-21 21:25:46 +0900

    [feat] Logger in random command

--- commands/random.js
+++ commands/random.js
@@ -1,9 +1,11 @@
 const {EmbedBuilder} = require('discord.js')
 const axios = require('axios')
 const {bojProblem} = require("../models/problem");
+const logger = require("../logger")
 
 async function getRandomProblem(attempts = 0) {
     if (attempts >= 5) {
+        logger.error("최대 요청 횟수 (5회) 초과 또는 알 수 없는 오류")
         return new bojProblem(-1, "알 수 없는 오류가 발생했습니다.", 0, [])
     }
     //TODO 현재는 숫자로 범위를 지정해줬는데, 나중에 백준 사이트를 통해 직접 문제 수를 받아야 함.
@@ -18,8 +20,7 @@ async function getRandomProblem(attempts = 0) {
 
     } catch (error) {
         //가끔 번호가 배정이 안된 경우가 있음
-        console.error(error.response.data);
-        console.error("Retrying...")
+        logger.error(`문제 요청 실패. 다시 요청 시도 (${attempts+1}번쨰 시도)`)
         //최대 5번 시도
         return await getRandomProblem(attempts + 1);
     }
@@ -30,10 +31,7 @@ module.exports = {
     name: '문제 랜덤 추천',
     async execute(message, userCommandStatus, args) {
         const randProblem = await getRandomProblem();
-        console.log(randProblem.problemId)
-        console.log(randProblem.title)
-        console.log(randProblem.getLevel())
-        console.log(randProblem.tags)
+        logger.info(`반환 성공 : ${message.author.id}에게 ${randProblem.problemId}번 문제 반환`)
 
         const randProblemMsg = randProblem.getEmbedMsg("랜덤 문제입니다.")
 
